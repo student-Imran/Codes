db325fc04d2faff45bad165cfe43d81d55c60c48
student-Imran <ahmedimranmd55@gmail.com>
2025-08-12 09:50:45 +0600

    CF-problem

---
+++ cf_Combination-Lock.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std; 
+
+void solve()
+{
+   int n;cin>>n;
+   if(n%2==0){
+    cout<<-1<<'\n';
+    return;
+   }
+   for(int i=n;i>=1;i--){
+    cout<<i<<" ";
+   }cout<<'\n';
+}  
+
+int main()
+{
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+
+
+  int t;
+  cin>>t;
+  while(t--)
+  {
+    solve();
+  }
+  return 0;
+
+}
+
